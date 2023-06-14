c5b185b5ae0e42d353c931ae4661dfa1a6e4448b
Ritesh Ranjan <riteshr.designs@gmail.com>
2023-06-14 12:49:22 +0000

    Added 0613 & 16K chatgpt models

--- lib/src/models/gpt_model.dart
+++ lib/src/models/gpt_model.dart
@@ -1,5 +1,8 @@
 enum GPTModel {
   gpt3p5turbo("gpt-3.5-turbo", 4000),
+  gpt3p5superturbo("gpt-3.5-turbo-0613", 4000),
+  gpt3p5turbo16k("gpt-3.5-turbo-16k", 16000),
+  gpt3p5superturbo16k("gpt-3.5-turbo-16k-0613", 16000),
   davinci("text-davinci-003", 4000),
   curie("text-curie-001", 2048),
   babbage("text-babbage-001", 2048),
